808097e083091d92774fce2f6bf653afb5418a64
Megumu Tokuda <megumu112851@gmail.com>
2022-04-05 23:40:20 +0900

    complete nav

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
 {
-    
+    const navis = document.querySelectorAll('header nav ul li a');
+    const targets = document.querySelectorAll('main div');
+    console.log(navis);
+    navis.forEach(nav=>{
+        nav.addEventListener('click', (e)=>{
+            e.preventDefault();
+            navis.forEach(nav=>{nav.classList.remove('current')});
+            const name = nav.dataset.nav;
+            targets.forEach(target=>{
+                if(target.id === name) target.classList.add('appear');
+            });
+            targets.forEach(target=>{
+                if(target.id !== name) target.classList.remove('appear');
+            });
+            nav.classList.add('current');
+        });
+    });
+
 }
